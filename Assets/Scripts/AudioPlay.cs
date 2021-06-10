d25e016770bb7478fcb98476e0be4a22e6424f6a
J-mung <xorb2298@gmail.com>
2021-06-11 04:40:01 +0900

    Dialog scene 수정 및 이미지 파일 추가

--- Assets/Scripts/AudioPlay.cs
+++ Assets/Scripts/AudioPlay.cs
@@ -1,20 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class AudioPlay : MonoBehaviour
 {
     public AudioSource sound;
     private Rect audiorect;
+    public GameObject switchOn, switchOff;
 
     // Start is called before the first frame update
     void Start()
     {
-        sound = gameObject.AddComponent<AudioSource>();
+        switchOn.SetActive(false);
     }
 
     public void BGMOnOff(bool onOff)
     {
-        sound.mute = !onOff;
+        bool onoffSwitch = gameObject.GetComponent<Toggle>().isOn;
+        if (onoffSwitch)
+        {
+            switchOn.SetActive(true);
+            switchOff.SetActive(false);
+        }
+        if (!onoffSwitch)
+        {
+            switchOn.SetActive(false);
+            switchOff.SetActive(true);
+        }
     }
 }
