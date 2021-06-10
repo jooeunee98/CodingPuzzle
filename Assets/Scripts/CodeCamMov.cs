548202898dd1e140be9eed4f3b51073c0490b828
jooeunee98 <51391097+jooeunee98@users.noreply.github.com>
2021-06-10 13:46:38 +0900

    맵 생성 일부 및 블럭존 코딩존 수정

---
+++ Assets/Scripts/CodeCamMov.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CodeCamMov : MonoBehaviour
+{
+
+    public float moveSpeed;
+    public Transform cam;
+
+    Vector2 prevPos = Vector2.zero;
+    float prevDistance = 0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //cam = Camera.main.transform;
+    }
+
+    public void OnDrag()
+    {
+        int touchCount = Input.touchCount;
+
+        if (touchCount == 1)
+        {
+            if(prevPos == Vector2.zero)
+            {
+                prevPos = Input.GetTouch(0).position;
+                return;
+            }
+
+            Vector2 dir = (Input.GetTouch(0).position - prevPos).normalized;
+            Vector3 vec = new Vector3(dir.x, 0, dir.y);
+
+            cam.position -= vec * moveSpeed * Time.deltaTime;
+            prevPos = Input.GetTouch(0).position;
+        }
+
+        else if (touchCount == 2)
+        {
+            if (prevDistance == 0)
+            {
+                prevDistance = Vector2.Distance(Input.GetTouch(0).position, Input.GetTouch(1).position);
+                return;
+            }
+
+            float curDistance = Vector2.Distance(Input.GetTouch(0).position, Input.GetTouch(1).position);
+            float move = prevDistance - curDistance;
+
+            Vector3 pos = cam.position;
+
+            if (move < 0) pos.y -= moveSpeed * Time.deltaTime * 10;
+            else if (move > 0) pos.y += moveSpeed * Time.deltaTime * 10;
+
+            cam.position = pos;
+            prevDistance = curDistance;
+        }
+    }
+
+    public void exitDrag()
+    {
+
+    }
+}
