71b236336276f9f29b454d33ae5da5647ca87033
J-mung <xorb2298@gmail.com>
2021-11-04 23:10:50 +0900

    사막 스테이지 1번의 기본 틀 구현

--- Assets/Scripts/CharacterManager.cs
+++ Assets/Scripts/CharacterManager.cs
@@ -5,9 +5,10 @@ using UnityEngine;
 public class CharacterManager : MonoBehaviour
 {
     // Start is called before the first frame update
+    // 캐릭터 및 별(골지점) 초기화 위치
     public float spawnPosX, spawnPosY, spawnPosZ;
     public float spawnRotX, spawnRotY, spawnRotZ;
-
+    
     void Start()
     {
         spawnPosY = 8;
@@ -22,8 +23,11 @@ public class CharacterManager : MonoBehaviour
 
     public void CharacterSpawn ()
     {
+        // 기존의 캐릭터 삭제
+        Destroy(GameObject.Find("Character"));
+
         // 프리펩 호출
-        GameObject prefab = Resources.Load("Prefabs/Walking") as GameObject;
+        GameObject prefab = Resources.Load("Prefabs/Walking") as GameObject;    // 캐릭터
         // 블루존(코딩존)에 생성할 프리펩 인스턴스화
         GameObject character = Instantiate(prefab) as GameObject;
         // mapBlocks 밑에다가 생성
